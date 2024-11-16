d635591f86d368919f0a51be997043284eef06d9
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-16 23:01:42 +0530

    creating cart entity

--- src/main/java/com/example/ShoppingCart_Hibernate/App.java
+++
@@ -1,6 +0,0 @@
-package com.example.ShoppingCart_Hibernate;
-
-public class App 
-{
-	
-}
---
+++ src/main/java/com/example/entity/Cart.java
@@ -0,0 +1,24 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String cartId;
+
+	public String getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(String cartId) {
+		this.cartId = cartId;
+	}
+	
+	
+}
