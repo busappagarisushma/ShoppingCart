00159d622965f788d56a26fe148fd75fb0e6ec65
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-17 22:08:51 +0530

    creating getters and setters

--- src/main/java/com/example/entity/Product.java
+++ src/main/java/com/example/entity/Product.java
@@ -1,5 +1,44 @@
 package com.example.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String productId;
+	private String name;
+	private String brand;
+	private String price;
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+
 
 }
